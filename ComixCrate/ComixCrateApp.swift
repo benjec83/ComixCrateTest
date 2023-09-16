f88971a9eb4eba7d16032bc8721feaf1c9576939
Ben Carney <ben@thistlenthorn.com>
2023-09-16 09:13:49 -0700

    Initial Commit

---
+++ ComixCrate/ComixCrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixCrateApp.swift
+//  ComixCrate
+//
+//  Created by Ben Carney on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ComixCrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
